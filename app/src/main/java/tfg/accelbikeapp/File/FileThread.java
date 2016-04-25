45ea008ce26d7cc6c500ea5df630f4bec4ce7aa3
alexis <mikeljulio@gmail.com>
2016-04-25 18:49:12 +0200

    Thread Bluetooth, File Guardar en carpetas **** Location OK*****

--- app/src/main/java/tfg/accelbikeapp/File/FileThread.java
+++ app/src/main/java/tfg/accelbikeapp/File/FileThread.java
@@ -1,10 +1,13 @@
 package tfg.accelbikeapp.File;
 
 import android.content.Context;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.Date;
 
 import tfg.accelbikeapp.Bluetooth.BluetoothThread;
+import tfg.accelbikeapp.GPS.CoordenadasThread;
 
 /**
  * Created by alexis on 22/04/16.
@@ -16,9 +19,11 @@ public class FileThread extends Thread {
     private BluetoothThread bluetooth;
     private ArrayList<Short> ejes;
     private Date fecha;
+    private Context ctx;
 
     public FileThread(Context context){
         super();
+        this.ctx = context;
         bluetooth = new BluetoothThread();
         //GPS TODO
         filemanager = new FileManager(context);
@@ -29,6 +34,8 @@ public class FileThread extends Thread {
 
         fecha = new Date();
 
+        CoordenadasThread coordenadas = new CoordenadasThread(ctx);
+
         filemanager.updateSesion("sesionAlexis");
 
         while (!Thread.interrupted()) {
@@ -38,7 +45,12 @@ public class FileThread extends Thread {
                 /*Log.i("X", Short.toString(ejes.get(0)));
                 Log.i("Y", Short.toString(ejes.get(1)));
                 Log.i("Z", Short.toString(ejes.get(2)));*/
-                filemanager.guardar(Short.toString(ejes.get(0)), Short.toString(ejes.get(1)), Short.toString(ejes.get(2)));
+                filemanager.guardar(Short.toString(ejes.get(0)), Short.toString(ejes.get(1)),
+                        Short.toString(ejes.get(2)));
+
+                Log.i("Coordenadas", "Hola");
+                coordenadas.getLocation();
+                Log.i("Coordenadas", "ME LA SUDAN");
             }
 
             try {
--- app/src/main/java/tfg/accelbikeapp/GPS/MyLocationListener.java
+++ app/src/main/java/tfg/accelbikeapp/GPS/MyLocationListener.java
@@ -3,13 +3,25 @@ package tfg.accelbikeapp.GPS;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 
 /**
  * Created by alexis on 25/04/16.
  */
 public class MyLocationListener implements LocationListener {
+
+    public MyLocationListener(){
+
+        Looper.prepare();
+
+    }
+
+
     @Override
     public void onLocationChanged(Location location) {
+        Log.i("LocationListener", Double.toString(location.getLatitude()));
+        Log.i("LocationListener", Double.toString(location.getLongitude()));
 
     }
 
